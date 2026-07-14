357b450f749d69e4d5e35d4e044d74241ede6aa2
Rajani Diwani <rayyan.rajan.dev@gmail.com>
2026-07-14 21:01:14 +0300

    fix: use default shadcn-vue PanelLeft icon for sidebar trigger

--- resources/js/components/AppSidebarHeader.vue
+++ resources/js/components/AppSidebarHeader.vue
@@ -213,7 +213,7 @@ function selectScope(key: string) {
         class="flex h-16 shrink-0 items-center justify-between gap-2 border-b border-sidebar-border/70 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4"
     >
         <div class="flex min-w-0 items-center gap-2 overflow-hidden">
-            <SidebarTrigger class="-ml-1 shrink-0" />
+            <SidebarTrigger />
             <template v-if="breadcrumbs && breadcrumbs.length > 0">
                 <Breadcrumbs :breadcrumbs="breadcrumbs" />
             </template>
