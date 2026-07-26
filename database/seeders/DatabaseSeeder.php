c5826dcb18f3c115c554c20575a70743b8a489c8
Rajani Diwani <rayyan.rajan.dev@gmail.com>
2026-07-26 23:16:42 +0300

    fix: replace clinical procedures with dispensary-appropriate list, remove theatre seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
             DskRadiologyClinicalCatalogSeeder::class,
             DskFormularyClinicalCatalogSeeder::class,
             DskClinicalClinicalCatalogSeeder::class,
-            DskTheatreClinicalCatalogSeeder::class,
         ]);
     }
 }
